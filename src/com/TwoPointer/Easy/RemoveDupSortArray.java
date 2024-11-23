c137aa48754d22566a992d7f1bc16d9e0711cb27
Madhura <msp24mp@gmail.com>
2024-11-23 12:37:14 +0530

    Two Pointer eg1

---
+++ src/com/TwoPointer/Easy/RemoveDupSortArray.java
@@ -0,0 +1,36 @@
+package com.TwoPointer.Easy;
+
+import java.util.*;
+
+public class RemoveDupSortArray {
+	
+	public static void main(String[] args) {
+		int[] nums= {1,1,2,3,3,4};
+		int k=removeDuplicates(nums);
+	}
+	
+	 public static int removeDuplicates(int[] nums) {
+		 
+		 //count of uniq elements
+		 int count=1;
+		 //pointers to navigate
+		 int a_pointer=1;
+		
+		 
+		 while(a_pointer<nums.length)
+		 {
+			 if(nums[a_pointer]!=nums[count-1])
+			 {
+			     nums[count]=nums[a_pointer];
+				count++;
+			 }
+			 a_pointer++;
+			
+		 }
+		 System.out.println(Arrays.toString(nums));
+		 	 
+		 return count;
+	        
+	    }
+
+}
