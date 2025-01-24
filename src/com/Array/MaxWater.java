653b7b526b295fa28e6a777183bf3326765f5620
Madhura <msp24mp@gmail.com>
2025-01-24 05:55:54 +0530

    day_28_29

--- src/com/Array/MaxWater.java
+++ src/com/Array/MaxWater.java
@@ -6,5 +6,40 @@ public class MaxWater {
 		// TODO Auto-generated method stub
 
 	}
+	
+	 public int maxArea(int[] height) {
+		 int apt=0;
+		 int bpt=height.length-1;
+		 int maxarea=0;
+		 
+		 while(apt<bpt)
+		 {
+			 int ht=Math.min(height[apt], height[bpt]);
+			 int area=ht*((bpt-apt));
+			 if(area>maxarea)
+			 {
+				 maxarea=area;
+			 }
+			 
+			 if(height[apt]<height[bpt])
+			 {
+				 apt++;
+			 }else
+			 {
+				 bpt--;
+			 }
+			 
+		 }
+		 
+		 return maxarea;
+	        
+	    }
+	 
+	 
+	 //2 points 
+	 //start point: 0 - last pt: n
+	 //area=min {h(0),h(n)} * (n-0)
+	 // Max=Maths
+	 //
 
 }
--- src/com/Array/Sum3Closest.java
+++ src/com/Array/Sum3Closest.java
@@ -1,10 +1,93 @@
   package com.Array;
 
+import java.util.Arrays;
+
 public class Sum3Closest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
+public int threeSumClosest(int[] nums, int target) {
+
+	int sum=0;
+	Arrays.sort(nums);
+	int min=Integer.MAX_VALUE/2;
+	
+	for(int i=0;i<nums.length-2;i++)
+	{
+		int b=i+1;
+		int c=nums.length-1;
+	while(b<c)
+	{
+		int s=nums[i]+nums[b]+nums[c];
+
+		if(Math.abs(min-target)>Math.abs(s-target))
+		{
+			sum=s;
+            System.out.println(sum);
+			min=s;
+		}
+		//min=Math.min(min, 2Math.abs(s)-Math.abs(target));
+				
+		//b++;
+        if(s<target)
+        {
+           b++;
+        }else if (s>target)
+        {
+		c--;
+        }
+        else
+        {
+            return s;
+        }
+	}	
+	}
+      return min;          
+    }
+
+
+public int threeSumClosestOpt(int[] nums, int target) {
+	
+	int sum=0;
+	Arrays.sort(nums);
+	int min=Integer.MAX_VALUE/2;
+	
+	for(int i=0;i<nums.length-2;i++)
+	{
+		int b=i+1;
+		int c=nums.length-1;
+	while(b<c)
+	{
+		int s=nums[i]+nums[b]+nums[c];
+
+		if(Math.abs(min-target)>Math.abs(s-target))
+		{
+			sum=s;
+           System.out.println(sum);
+			min=s;
+		}
+		//min=Math.min(min, 2Math.abs(s)-Math.abs(target));
+				
+		//b++;
+       if(s<target)
+       {
+          b++;
+       }else if (s>target)
+       {
+		c--;
+       }
+       else
+       {
+           return s;
+       }
+	}	
+	}
+     return min;  
+
+       
+   }
 
 }
